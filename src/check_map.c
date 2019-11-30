add1489de8a92047e106aec402d24185f59c3531
Drahff Orange dartwing <dorange-@il-b3.21-school.ru>
2019-11-30 15:02:30 +0300

    fix ft_check_map

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: dorange- <dorange-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 16:44:43 by dorange-          #+#    #+#             */
-/*   Updated: 2019/11/30 14:43:56 by dorange-         ###   ########.fr       */
+/*   Updated: 2019/11/30 15:01:33 by dorange-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,31 @@
 /*
 ** **************************************************************************
 **	void ft_check_map(t_map *map)
+**	Function to check map's item
+** **************************************************************************
+*/
+
+static int		ft_check_map_item(t_map *map, int i, int j, int empty_count)
+{
+	if ((i == 0 || i == (map->m_wid - 1)) ||
+		(j == 0 || j == (map->m_hei - 1)))
+	{
+		if (map->map[j * map->m_wid + i] == 0 ||
+			map->map[j * map->m_wid + i] >= 20)
+			ft_error("Invalid map");
+	}
+	else
+	{
+		if (map->map[j * map->m_wid + i] == 0)
+			empty_count++;
+	}
+	return (empty_count);
+}
+
+/*
+** **************************************************************************
+**	void ft_check_map(t_map *map)
 **	Function to check maps
-**
-**	!: function ft_check_map has 28 lines
 ** **************************************************************************
 */
 
@@ -36,15 +58,7 @@ void		ft_check_map(t_map *map)
 		i = 0;
 		while (i < map->m_wid)
 		{
-			if ((i == 0 || i == (map->m_wid - 1)) ||
-				(j == 0 || j == (map->m_hei - 1)))
-			{
-				if (map->map[j * map->m_wid + i] == 0 ||
-					map->map[j * map->m_wid + i] >= 20)
-					ft_error("Invalid map");
-			}
-			else
-				empty_count++;
+			empty_count = ft_check_map_item(map, i, j, empty_count);
 			i++;
 		}
 		j++;
